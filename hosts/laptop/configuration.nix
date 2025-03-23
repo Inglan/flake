1e9f9f24344577021b074990c77cbff87ffaf73f
Ingo Wolf <me@ingowolf.au>
2025-03-24 08:59:36 +1100

    add bluetooth

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -146,6 +146,8 @@
     };
   };
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
 
   # Some programs need SUID wrappers, can be configured further or are
