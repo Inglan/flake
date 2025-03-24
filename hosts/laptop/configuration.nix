44b725d1745d682b57755e6ac400914c7f65409e
Ingo Wolf <me@ingowolf.au>
2025-03-24 17:17:11 +1100

    enable plymouth

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -17,6 +17,18 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
+    plymouth.enable = true;
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 
 
