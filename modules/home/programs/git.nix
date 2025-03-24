3ac298d269d2d38e2474f07fc77dff7086f76400
Ingo Wolf <me@ingowolf.au>
2025-03-24 17:13:10 +1100

    fix git

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -15,7 +15,7 @@
       };
 
       user = {
-        signingKey = "...";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFlaUR/043CCjtUXWjsAjjJE/cUeeJKiDtDr22oxcgT";
       };
     };
   };
