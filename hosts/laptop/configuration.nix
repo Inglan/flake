4f88083ee23eaf5c0bbfeb82d46f4edeb088d909
Ingo Wolf <me@ingowolf.au>
2025-03-24 17:32:00 +1100

    remove tide configure from fish

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -113,7 +113,7 @@
     enable = true;
     interactiveShellInit = ''
       set -g fish_greeting
-      tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=No
+      #tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=No
     '';
   };
 
