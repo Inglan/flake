9ff4f5f692e596b4ee439dd05e908e219f11470e
Ingo Wolf <me@ingowolf.au>
2025-03-24 15:44:55 +1100

    enable 1password commit signing

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -1,7 +1,22 @@
-{ config, pkgs, ... } : {
+{ config, lib, pkgs, ... } : {
   programs.git = {
     enable = true;
     userName = "Ingo Wolf";
     userEmail = "me@ingowolf.au";
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+
+      user = {
+        signingKey = "...";
+      };
+    };
   };
 }
