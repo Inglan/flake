3c5ddc7382c8880d690b0b70424b2927e67d7ad1
Ingo Wolf <me@ingowolf.au>
2025-03-27 10:58:49 +1100

    swap d and f because it feels right :)

--- modules/system/keyboard/keyd.nix
+++ modules/system/keyboard/keyd.nix
@@ -15,8 +15,8 @@
             j = "down";
             k = "up";
             l = "right";
-            d = "C-tab";
-            f = "C-S-tab";
+            f = "C-tab";
+            d = "C-S-tab";
           };
         };
       };
