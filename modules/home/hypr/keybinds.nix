3518b27aa445c925d7725d2402e2042cf3942625
Ingo Wolf <me@ingowolf.au>
2025-04-06 11:49:01 +1000

    move keybinds to file

---
+++ modules/home/hypr/keybinds.nix
@@ -0,0 +1,25 @@
+{...}: {
+  bind =
+    [
+      "$mod, Return, exec, foot"
+      "$mod, B, exec, firefox"
+      ", Print, exec, grimblast copy area"
+      "$mod, Super_L, exec, $menu"
+    ]
+    ++ (
+      # workspaces
+      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+      builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        )
+        9)
+    );
+  bindm = [
+    "$mod, mouse:272, movewindow"
+    "$mod, mouse:273, resizewindow"
+  ];
+}
