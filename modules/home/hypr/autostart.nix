46566258cf6f7646d56fe11413f66d0be810b58f
Ingo Wolf <me@ingowolf.au>
2025-04-06 13:23:15 +1000

    autostart ags

--- modules/home/hypr/autostart.nix
+++ modules/home/hypr/autostart.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "systemctl --user start hyprpolkitagent"
+      "ags run -d ~/.config/ags"
       "swww-deamon"
       "1password --silent"
       "zen"
